47f60c69b9475d646fc76ee3a8d65383bd622854
duhai <Starteos@123>
2021-11-03 15:10:13 +0800

    readme

--- README.md
+++ README.md
@@ -1,3 +1,25 @@
 # AutoPreload
     kapt "com.github.Jackduhai.AutoPreload:processor:1.0.1.8"
     implementation 'com.github.Jackduhai.AutoPreload:autopreload:1.0.1.8'
+
+#普通对象预加载
+@AutoPreload
+public class Load2 {
+    @LoadMethod
+    public void loadMyMessagePre2(){
+        System.out.println("==========loadMyMessagePre2============");
+    }
+}
+
+
+#单例对象预加载
+@AutoPreload
+object LoadNews {
+
+    @LoadMethod
+    fun loadMyMessagePre(){
+        println("==========loadMyMessagePre============")
+    }
+
+
+}
--- app/src/main/java/com/jack/autopreload2/LoadNews.kt
+++ app/src/main/java/com/jack/autopreload2/LoadNews.kt
@@ -1,15 +1,15 @@
 package com.jack.autopreload2
 
-//import com.jack.annotation.AutoPreload
-//import com.jack.annotation.LoadMethod
-//
-//@AutoPreload
-//object LoadNews {
-//
-//    @LoadMethod
-//    fun loadMyMessagePre(){
-//        println("==========loadMyMessagePre============")
-//    }
-//
-//
-//}
+import com.jack.annotation.AutoPreload
+import com.jack.annotation.LoadMethod
+
+@AutoPreload
+object LoadNews {
+
+    @LoadMethod
+    fun loadMyMessagePre(){
+        println("==========loadMyMessagePre============")
+    }
+
+
+}
